ffb83d57e6c74984b176070a022fe6849c3c2ff0
Alex <lyosha85@gmail.com>
2016-02-10 14:00:16 +0900

    rspec boilerplate

---
+++ spec/app_spec.rb
@@ -0,0 +1,13 @@
+# spec/app_spec.rb
+require File.expand_path '../spec_helper.rb', __FILE__
+
+describe "My Sinatra Application" do
+  it "should allow accessing the home page" do
+    get '/'
+    # Rspec 2.x
+    expect(last_response).to be_ok
+
+    # Rspec 1.x
+    last_response.should be_ok
+  end
+end
